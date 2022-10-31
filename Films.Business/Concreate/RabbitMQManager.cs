582037dca652a0f8d0f0f5fa017ca4432e7ba10a
DESKTOP-71IV4V1\Bilal Taş <bilaltas9@gmail.com>
2022-11-01 02:56:58 +0300

    feature : add rabbitmq service.

---
+++ Films.Business/Concreate/RabbitMQManager.cs
@@ -0,0 +1,30 @@
+﻿using Films.Business.Abstract;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Films.Business.Concreate
+{
+    public class RabbitMQManager : IRabbitMqService
+    {
+        public RabbitMQManager()
+        {
+
+        }
+        public void DeclareQue()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest" };
+            IConnection connection = factory.CreateConnection();
+            IModel channel = connection.CreateModel();
+            channel.QueueDeclare("FilmQueue");
+        }
+
+        public void SendMessageToQueue(string queMessage)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest" };
+            IConnection connection = factory.CreateConnection();
+            IModel channel = connection.CreateModel();
+            var body = Encoding.UTF8.GetBytes(queMessage);
+            channel.BasicPublish(exchange: "", routingKey: "FilmQueue", body: body);
+        }
+    }
+}
